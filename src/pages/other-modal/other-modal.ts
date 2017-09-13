cb6247335a91fb0599467057ef116f545c27ecd5
Brad Grzesiak <brad@bendyworks.com>
2017-09-13 16:13:23 -0500

    Dismiss works

--- src/pages/other-modal/other-modal.ts
+++ src/pages/other-modal/other-modal.ts
@@ -1,11 +1,5 @@
 import { Component } from "@angular/core";
-import {
-  NavController,
-  NavParams,
-  ModalController,
-  ViewController,
-  IonicPage
-} from "ionic-angular";
+import { NavController, NavParams, IonicPage } from "ionic-angular";
 
 /**
  * Generated class for the OtherModalPage page.
@@ -20,21 +14,13 @@ import {
   templateUrl: "other-modal.html"
 })
 export class OtherModalPage {
-  constructor(
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public modalCtrl: ModalController,
-    public viewCtrl: ViewController
-  ) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad OtherModalPage");
   }
 
   doDismiss() {
-    // console.log(JSON.stringify(this.navCtrl));
-    // console.log(JSON.stringify(this.viewCtrl));
-    console.log(JSON.stringify(this.navParams));
-    this.viewCtrl.dismiss();
+    this.navCtrl["rootParams"].viewCtrl.dismiss();
   }
 }
